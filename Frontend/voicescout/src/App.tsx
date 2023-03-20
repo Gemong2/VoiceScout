3d389648dd3a52d55c46ffb5e2685123e20a2688
kjskjs356 <kjskjs356@gmail.com>
2023-03-20 15:24:23 +0900

    [FE]:[FIX] 헤더 푸터 작업, 기타 페이지 생성

--- Frontend/voicescout/src/App.tsx
+++ Frontend/voicescout/src/App.tsx
@@ -1,16 +1,25 @@
-import { Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import HeaderFooterPage from "./pages/HeaderFooterePage";
 import HeaderPage from "./pages/HeaderPage";
 import Main from "pages/Main";
+import CrimeType from "pages/CrimeType";
+import CallRoom from "pages/CallRoom";
+import Quiz from "pages/Quiz";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route element={<HeaderFooterPage />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/crime-type*" element={<CrimeType />} />
+          <Route path="/call-room" element={<CallRoom />} />
+        </Route>
         <Route element={<HeaderPage />}>
           <Route path="/" element={<Main />} />
+          <Route path="/quiz/*" element={<Quiz />} />
         </Route>
       </Routes>
     </>
