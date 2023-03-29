e0f566545ea44c5ac408c81da1123509c97075fa
hosanyoon <yoonhosan@naver.com>
2023-03-29 14:15:57 +0900

    [COMMON] : [ADD] 스켈레톤 코드

---
+++ Backend/voicescout/src/test/java/com/ssafy/voicescout/VoicescoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.voicescout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicescoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
