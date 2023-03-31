596f8badbc37d3bd07ec120f7bc24a88c9d9327d
gemong <gemong2@naver.com>
2023-03-31 16:10:53 +0900

    [FE] : [FIX] 범죄 유형 페이지 범인 문자 수정

--- Frontend/voicescout/src/components/crime-type/CriminalLetter.tsx
+++ Frontend/voicescout/src/components/crime-type/CriminalLetter.tsx
@@ -16,7 +16,7 @@ export default function CriminalLetter() {
   // 모달창 노출 여부 state
   const [modalOpen, setModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState<React.ReactNode>(null);
-
+  const [content, setContent] = useState<string>("");
 
   // 모달창 노출
   const showModal = () => {
@@ -26,82 +26,13 @@ export default function CriminalLetter() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const handleButtonClick = (imageSrc: string) => {
-    setModalContent(<img src={imageSrc} alt="modal-image" />);
+    setModalContent(<img className={style.modal_img} src={imageSrc} alt="modal-image" />);
     setModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setModalOpen(false);
   };
-  // const [buttonState, setButtonState] = useState(0);
-  // const buttonTab = (index:number) => {
-  //   setButtonState(index);
-  // }
-  // const Button_One = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modal_text}>상세 내용</div>
-  //       <div className={style.modalsize}>
-  //         <img className={style.modal_img} src={buttonone} alt="buttonone" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // const Button_Two = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modalsize}>
-  //       <img className={style.modal_img} src={buttontwo} alt="buttontwo" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // const Button_Three = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modalsize}>
-  //       <img className={style.modal_img} src={buttonthree} alt="buttonthree" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // const Button_Four = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modalsize}>
-  //       <img className={style.modal_img} src={buttonfour} alt="buttonfour" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // const Button_Five = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modalsize}>
-  //       <img className={style.modal_img} src={buttonfive} alt="buttonfive" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  // const Button_Six = () => {
-  //   return (
-  //     <div className={style.modal_square}>
-  //       <div className={style.modalsize}>
-  //       <img className={style.modal_img} src={buttonsix} alt="buttonsix" />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  let content;
-
-  // if (buttonState === 1 ) content = <Button_One />
-  // else if (buttonState === 2) content = <Button_Two />
-  // else if (buttonState === 3) content = <Button_Three />
-  // else if (buttonState === 4) content = <Button_Four />
-  // else if (buttonState === 5) content = <Button_Five />
-  // else if (buttonState === 6) content = <Button_Six />
-
-  
 
   useEffect(() => {
     const scrollHandler = (e: WheelEvent) => {
@@ -125,31 +56,30 @@ export default function CriminalLetter() {
       }
     };
   }, [scrollRef]);
-
+  
   return (
     <div className={style.container}>
       <div className={style.wrap}>
         <img className={style.img_size} src={guide} alt="guide" />
         <div className={style.chat +" "+ style.ch1}>
-          <div className={style.textbox}>아래 항목을 클릭해서 상세 문자내용을 확인해보세요
-            </div>
+          <div className={style.textbox}>아래 항목을 클릭해서 상세 문자내용을 확인해보세요</div>
           </div>
         </div>
         <div className={style.square}>
-          <div ref={scrollRef} className={style.square_rule}>
-          <button onClick={() => handleButtonClick("img/button_one.jpg")}>대출사기문자</button>
-            </div>
-            {modalOpen && <CriminalLetterModal setModalOpen={setModalOpen} content={content} />}
-      </div>      
+        <div ref={scrollRef} className={style.square_rule}>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttonone)}>대출사기문자</button>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttontwo)}>물품결제문자</button>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttonthree)}>생계지원대출</button>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttonfour)}>정부지원대환대출</button>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttonfive)}>긴급재난지원대출</button>
+          <button className={style.tabbutton +" "+ style.tabbutton_on} onClick={() => handleButtonClick(buttonsix)}>대출사기문자</button>
+        </div>
+      </div>  
+      {modalOpen && (
+          <CriminalLetterModal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+          {modalContent}
+        </CriminalLetterModal>
+        )}    
     </div>
   )
 }
-
-{/* <button className={buttonState === 2 ? style.tabbutton_on + " " + style.tabbutton: style.tabbutton} onClick={() => buttonTab(2)}>물품결제문자</button>
-<button className={buttonState === 3 ? style.tabbutton_on + " " + style.tabbutton: style.tabbutton} onClick={() => buttonTab(3)}>생계지원대출</button>
-<button className={buttonState === 4 ? style.tabbutton_on + " " + style.tabbutton: style.tabbutton} onClick={() => buttonTab(4)}>정부지원<br />대환대출</button>
-<button className={buttonState === 5 ? style.tabbutton_on + " " + style.tabbutton: style.tabbutton} onClick={() => buttonTab(5)}>긴급재난<br />지원대출</button>
-<button className={buttonState === 6 ? style.tabbutton_on + " " + style.tabbutton: style.tabbutton} onClick={() => buttonTab(6)}>대출사기형</button> 
-<div>{content}</div>
-<button onClick={showModal}>모달 띄우기</button>
-{modalOpen && <CriminalLetterModal setModalOpen={setModalOpen} content={content} /> */}
