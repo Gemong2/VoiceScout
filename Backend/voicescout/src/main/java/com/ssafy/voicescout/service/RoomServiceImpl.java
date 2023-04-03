5682efac35d4f6a6f5667c160c339ae029f777bb
hosanyoon <yoonhosan@naver.com>
2023-04-03 14:11:25 +0900

    [BE]:[FEAT] 방생성, 수정, 삭제, 목록조회

---
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/service/RoomServiceImpl.java
@@ -0,0 +1,72 @@
+package com.ssafy.voicescout.service;
+
+import com.ssafy.voicescout.dto.RoomDto;
+import com.ssafy.voicescout.entity.Room;
+import com.ssafy.voicescout.repository.RoomRepository;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements RoomService {
+  private final RoomRepository roomRepository;
+
+  @Override
+  @Transactional
+  public RoomDto createRoom(RoomDto roomReqDto) {
+    Room room = Room.builder()
+        .title(roomReqDto.getTitle())
+        .link(roomReqDto.getLink())
+        .password(roomReqDto.getPassword())
+        .registDate(LocalDateTime.now())
+        .participant(roomReqDto.getParticipant())
+        .typeId(roomReqDto.getTypeId())
+        .build();
+    Room createdRoom = roomRepository.save(room);
+    roomReqDto.setSeq(createdRoom.getSeq());
+    log.info("[createRoom] : 방 생성 완료, RoomSeq : {}", roomReqDto.getSeq());
+    return roomReqDto;
+  }
+  @Override
+  @Transactional
+  public RoomDto updateRoom(RoomDto roomReqDto) {
+    Room room =  roomRepository.getById(roomReqDto.getSeq());
+    room.updateRoom(roomReqDto);
+    log.info("[createRoom] : 방 수정 완료, RoomSeq : {}", roomReqDto.getSeq());
+    return roomReqDto;
+  }
+
+  @Override
+  @Transactional
+  public void deleteRoom(long roomSeq) {
+    roomRepository.deleteById(roomSeq);
+    log.info("[deleteRoom] : 방 삭제 완료, RoomSeq : {}", roomSeq);
+  }
+
+  @Override
+  @Transactional
+  public List<RoomDto> getRooms() {
+    List<Room> rooms = roomRepository.findAll();
+    List<RoomDto> roomsDto = new ArrayList<>();
+    for (Room room :  rooms) {
+      RoomDto roomDto = RoomDto.builder()
+          .seq(room.getSeq())
+          .title(room.getLink())
+          .password(room.getPassword())
+          .participant(room.getParticipant())
+          .link(room.getLink())
+          .locked(room.isLocked())
+          .registDate(room.getRegistDate())
+          .typeId(room.getTypeId())
+          .build();
+      roomsDto.add(roomDto);
+    }
+    return roomsDto;
+  }
+}
