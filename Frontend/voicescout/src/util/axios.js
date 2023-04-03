034e07089a656df8370518323a07ba86d10b5370
kjskjs356 <kjskjs356@gmail.com>
2023-04-03 16:28:19 +0900

    [FE] : [FIX] 서버 API통신 주소 변경

--- Frontend/voicescout/src/util/axios.js
+++ Frontend/voicescout/src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $ = axios.create({
-  baseURL: "https://j8a404.p.ssafy.io/api",
+  baseURL: "https://j8a404.p.ssafy.io",
   // baseURL: "http://localhost:4433/api",
   headers: {
     "Content-Type": "application/json",
