99c8726e9ea814cb1cbf65ddfec4a4b5fdac92fe
박서영 <manchott@naver.com>
2023-04-04 15:49:00 +0900

    [AI] : [FIX] import 오류 수정

--- Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
@@ -13,6 +13,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
