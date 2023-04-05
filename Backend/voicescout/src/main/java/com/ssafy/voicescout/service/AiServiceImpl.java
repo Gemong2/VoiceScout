53359db953106d5bdad4c57fd29e54ff2813f8e2
박서영 <manchott@naver.com>
2023-04-05 16:56:04 +0900

    Update AiServiceImpl.java

--- Backend/voicescout/src/main/java/com/ssafy/voicescout/service/AiServiceImpl.java
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/service/AiServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssafy.voicescout.service;
 import com.ssafy.voicescout.dto.AiReqDto;
 import com.ssafy.voicescout.dto.AiResDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AiServiceImpl implements AiService {
@@ -26,6 +28,7 @@ public class AiServiceImpl implements AiService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         String jsonString = "{\"msg\":\"" + msg + "\"}";
         HttpEntity<String> requestBody = new HttpEntity<>(jsonString, headers);
+        log.info("[checkMessage] : 값 확인, requestBody : {}", requestBody);
         ResponseEntity<AiResDto> response = restTemplate.postForEntity(apiUrl, requestBody, AiResDto.class);
         if (response.getStatusCodeValue() == 200){
             return response.getBody();
