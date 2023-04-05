da878d15423d753d2b2f0985ff908ff131a1eb45
hosanyoon <yoonhosan@naver.com>
2023-04-05 16:52:46 +0900

    [BE]:[FIX] AiController수정

--- Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
@@ -20,6 +20,7 @@ public class AiController {
   private final SimpMessagingTemplate simpMessagingTemplate;
   private final AiServiceImpl aiService;
   private static final String OUT_KEY = "get-out";
+  private static final String UPDATE_KEY = "update-room";
 
   @MessageMapping("/ai")
   //@SendTo("topic") 구독자들에게 보내기
@@ -32,6 +33,9 @@ public class AiController {
     if(OUT_KEY.equals(aiReqDto.getMsg())) {
       aiResDto = AiResDto.builder().prediction(2).build();
       log.info("[sendAi] : 방삭제 수신, prediction : {}", aiResDto.getPrediction());
+    } else if(UPDATE_KEY.equals(aiReqDto.getMsg())){
+      aiResDto = AiResDto.builder().prediction(3).build();
+      log.info("[sendAi] : 방수정 수신, prediction : {}", aiResDto.getPrediction());
     } else {
       aiResDto = aiService.checkMessage(aiReqDto);
       log.info("[sendAi] : prediction 수신 성공, prediction : {}", aiResDto.getPrediction());
