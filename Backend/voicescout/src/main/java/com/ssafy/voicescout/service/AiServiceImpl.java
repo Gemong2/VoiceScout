a9e3bbe61d0cd5cb4ce31345e412eeee87b1643d
조웅희 <ruh2034@naver.com>
2023-04-05 23:17:15 +0900

    [BE] : [FIX] Update AiServiceImpl.java

--- Backend/voicescout/src/main/java/com/ssafy/voicescout/service/AiServiceImpl.java
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/service/AiServiceImpl.java
@@ -23,7 +23,7 @@ public class AiServiceImpl implements AiService {
 
     @Override
     public AiResDto checkMessage(AiReqDto msg) {
-        String apiUrl = "https://j8a404.p.ssafy.io/ai/classification/";
+        String apiUrl = "http://ai-app:8000/classification/";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         String jsonString = "{\"msg\":\"" + msg + "\"}";
