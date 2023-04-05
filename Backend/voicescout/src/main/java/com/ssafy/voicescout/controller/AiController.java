16397b7e404b3415589ef48edba7e8f8fe8f9e41
hosanyoon <yoonhosan@naver.com>
2023-04-05 15:50:02 +0900

    [BE]:[FEAT] AiController 방삭제기능 추가

--- Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
+++ Backend/voicescout/src/main/java/com/ssafy/voicescout/controller/AiController.java
@@ -7,12 +7,9 @@ import com.ssafy.voicescout.dto.InteractionDto;
 import com.ssafy.voicescout.service.AiServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AiController {
   private final SimpMessagingTemplate simpMessagingTemplate;
   private final AiServiceImpl aiService;
-  private static String OUT_KEY = "get-out";
+  private static final String OUT_KEY = "get-out";
 
   @MessageMapping("/ai")
   //@SendTo("topic") 구독자들에게 보내기
@@ -31,14 +28,14 @@ public class AiController {
     AiReqDto aiReqDto = AiReqDto.builder()
         .msg(interactionDto.getMessage())
         .build();
+    AiResDto aiResDto;
     if(OUT_KEY.equals(aiReqDto.getMsg())) {
-      AiResDto aiResDto = AiResDto.builder().prediction(2).build();
+      aiResDto = AiResDto.builder().prediction(2).build();
       log.info("[sendAi] : 방삭제 수신, predition : {}", aiResDto.getPrediction());
-      simpMessagingTemplate.convertAndSend("/ai/" + interactionDto.getLink(), aiResDto);
     } else {
-      AiResDto aiResDto = aiService.checkMessage(aiReqDto);
+      aiResDto = aiService.checkMessage(aiReqDto);
       log.info("[sendAi] : predition 수신 성공, predition : {}", aiResDto.getPrediction());
-      simpMessagingTemplate.convertAndSend("/ai/" + interactionDto.getLink(), aiResDto);
     }
+    simpMessagingTemplate.convertAndSend("/ai/" + interactionDto.getLink(), aiResDto);
   }
 }
